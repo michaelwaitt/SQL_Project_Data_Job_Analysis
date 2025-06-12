634f4724852e845dbfab06b43635ae7b0fe945ba
mwaitt <michaelwaitt@gmail.com>
2025-06-12 10:14:37 -0500

    1st push
    Delete later

---
+++ sql_project/sql_course.session.sql
@@ -0,0 +1,39 @@
+-- CTE for combining job postings from January, February, and March
+WITH combined_job_postings AS (
+    SELECT job_id, job_posted_date
+    FROM january_jobs
+    UNION ALL
+    SELECT job_id, job_posted_date
+    FROM february_jobs
+    UNION ALL
+    SELECT job_id, job_posted_date
+    FROM march_jobs
+),
+-- CTE for calculating monthly skill demand based on the combined postings
+monthly_skill_demand AS (
+    SELECT
+        skills_dim.skills,  
+        EXTRACT(YEAR FROM combined_job_postings.job_posted_date) AS year,  
+        EXTRACT(MONTH FROM combined_job_postings.job_posted_date) AS month,  
+        COUNT(combined_job_postings.job_id) AS postings_count 
+    FROM
+        combined_job_postings
+    INNER JOIN skills_job_dim ON combined_job_postings.job_id = skills_job_dim.job_id  
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id  
+    GROUP BY
+        skills_dim.skills, 
+        year, 
+        month
+)
+-- Main query to display the demand for each skill during the first quarter
+SELECT
+    skills,  
+    year,  
+    month,  
+    postings_count 
+FROM
+    monthly_skill_demand
+ORDER BY
+    skills, 
+    year,
+    month;  
